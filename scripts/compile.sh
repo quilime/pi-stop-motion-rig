da836cee169922027be70a4072dfebbfdfc48ec0
Gabriel Dunne <gdunne@quilime.com>
2024-02-19 21:55:31 -0800

    Compile

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,6 +3,7 @@
 set -x
 
 ROOT='/home/admin/pi-stop-motion-rig'
-MOVIE_NAME=$1
+DATE=`date +%F_%H-%M-%S`
+FILE_NAME=${1:-$DATE}
 
-ffmpeg -framerate 6 -pattern_type glob -i "$ROOT/frames/*.jpg" -c:v libx264 -crf 27 -preset ultrafast "$ROOT/movies/$1.mp4"
+ffmpeg -framerate 6 -pattern_type glob -i "$ROOT/frames/*.jpg" -c:v libx264 -crf 27 -preset ultrafast "$ROOT/movies/$FILE_NAME.mp4"
