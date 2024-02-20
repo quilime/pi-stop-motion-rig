c4e813a744ba6524194b55c0b366c813afa09b88
Gabriel Dunne <gdunne@quilime.com>
2024-02-19 21:06:06 -0800

    Updating compile

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,8 +2,8 @@
 
 set -x
 
+FRAMERATE="${1:-6}" # default framerate to 6
 ROOT='/home/admin/pi-stop-motion-rig'
 DATE=`date +%F_%H-%M-%S`
 
-ffmpeg -framerate 6 -pattern_type glob -i "$ROOT/frames/*.jpg" -c:v libx264 -crf 27 -preset ultrafast "$ROOT/movies/$DATE.mp4"
-
+ffmpeg -framerate $FRAMERATE -pattern_type glob -i "$ROOT/frames/*.jpg" -c:v libx264 -crf 27 -preset ultrafast "$ROOT/movies/$DATE.mp4"
