5953f4998050c2af2884db6c8e924ae682a92725
Gabriel Dunne <gdunne@quilime.com>
2024-02-20 22:18:41 -0800

    Server and src

--- public/index.php
+++ public/index.php
@@ -1,19 +1,17 @@
 <?php
 
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
+
+$frames = './frames';
+$movies = './movies';
+
 function human_filesize($bytes, $decimals = 2) {
-	    $factor = floor((strlen($bytes) - 1) / 3);
-	        if ($factor > 0) $sz = 'KMGT';
-	        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . " " . @$sz[$factor - 1] . 'B';
+	$factor = floor((strlen($bytes) - 1) / 3);
+	if ($factor > 0) $sz = 'KMGT';
+		return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . " " . @$sz[$factor - 1] . 'B';
 }
-
-
-function formatBytes($bytes, $precision = 2) { 
-	    $units = array('B', 'KB', 'MB', 'GB', 'TB'); 
-	        $bytes = max($bytes, 0); 
-	        $pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
-		    $pow = min($pow, count($units) - 1); 
-                    return round($bytes, $precision) . $units[$pow]; 
-    } 
 
 ?>
 <html>
@@ -39,26 +37,17 @@ li {
 </head>
 <body>
 <h1>AnimatorPi.local</h1>
-<?php 
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-$frames = './frames';
-$movies = './movies';
-?>
-
-
 <ul>
 <?php foreach (array_reverse(glob($movies."/*.mp4")) as $filename) { ?>
 	<li>
-	<video width="320" height="240" controls>
+	<video width="320" height="240" controls loop>
 	<source src="<?php echo $filename ?>" type="video/mp4">
 </video>
 <br />
-	<a download href="<?php echo $filename ?>"><?php echo basename($filename) ?></a> 
-(<?php echo human_filesize(filesize($filename)); ?>)
+	<a download href="<?php echo $filename ?>">
+		<?php echo basename($filename) ?>
+	</a> 
+	(<?php echo human_filesize(filesize($filename)); ?>)
 	</li>
 <?php } ?>
 </ul>
